53258adc967a780001e3c06a4abf8a0c7174674c
JCAmorim11 <juliana.carol.amorim@gmail.com>
2022-10-30 17:15:53 -0300

    funcoes super()

--- polygon/family.py
+++ polygon/family.py
@@ -1,9 +1,14 @@
 class Parent:
-    def __init__(self):
-        print('PARENT')
-
-class Child(Parent):
+    def __init__(self, name):
+        print('PARENT', name)
+class Parent2:
+    def __init__(self, name):
+        print('Parent2 oldy', name)
+class Child(Parent2, Parent):
     def __init__(self):
         print('child')
+        Parent.__init__(self,'max')
+        Parent2.__init__(self,'stom')
 
 child = Child()
+print(Child.__mro__)
